598caa7ad4b69dac193ad2701ff90979e1e63fd1
Modeus <blastachanel@gmail.com>
2025-05-08 14:04:52 +0300

    feat: fish rod animation setup

--- Assets/Scripts/GameProcess/Interactions/FishingRod.cs
+++ Assets/Scripts/GameProcess/Interactions/FishingRod.cs
@@ -1,28 +1,38 @@
 ﻿using System.Collections.Generic;
+using DG.Tweening;
 using GameProcess.MiniGame;
+using Player;
 using Player.FishStorage;
 using UnityEngine;
 using Zenject;
 
 namespace GameProcess.Interactions
 {
-    [RequireComponent(typeof(Animator))]
     public class FishingRod : MonoBehaviour, IInteractable
     {
-        [Inject] private Animator _animator;
+        [Inject] private ResourceManager _resourceManager;
 
         [SerializeField] private MiniGameSetup miniGames;
         [SerializeField] private FishSetup fishSetup;
-
+        
+        [SerializeField] private Transform fishRodTransform;
         [SerializeField] private Transform firstFishingRodElement;
         [SerializeField] private Transform secondFishingRodElement;
         
-        private Transform _cameraTransform;
+        [SerializeField] private float focusDuration = 0.5f;
         
         private List<Transform> _rodChilds;
 
+        private Sequence _sequence;
+        
+        private Vector3 _startPosition;
+        private Vector3 _startRotation;
+        
         private void Awake()
         {
+            _startPosition = fishRodTransform.localPosition;
+            _startRotation = fishRodTransform.localRotation.eulerAngles;
+            
             _rodChilds = new List<Transform>();
 
             for (int i = 0; i < transform.childCount; i++)
@@ -33,16 +43,23 @@ namespace GameProcess.Interactions
         
         public void Interact()
         {
+            if (!ValidateMiniGameStart())
+                return;
+            
             ToggleChildFocus(false);
         }
 
         public void Focus()
         {
+            FocusAnimation();
+            
             ToggleChildFocus(true);
         }
 
         public void Unfocus()
         {
+            UnfocusAnimation();
+            
             ToggleChildFocus(false);
         }
         
@@ -52,6 +69,48 @@ namespace GameProcess.Interactions
             {
                 child.gameObject.layer = focus ? LayerMask.NameToLayer("Interactable") : LayerMask.NameToLayer("Default");
             }
+        }
+
+        private bool ValidateMiniGameStart()
+        {
+            if (_resourceManager.FishBaits == 0)
+                return false;
+            
+            return true;
+        }
+
+        private void FocusAnimation()
+        {
+            fishRodTransform?.DOKill();
+            _sequence?.Kill();
+            
+            _sequence = DOTween.Sequence();
+
+            _sequence.Append(fishRodTransform.DOLocalMove(new Vector3(11.743f, 9.72799969f, 8.55599976f), focusDuration / 2));
+            _sequence.Append(fishRodTransform.DOLocalMove(new Vector3(11.7919998f, 9.96899986f, 8.21500015f),
+                focusDuration / 2));
+
+            _sequence.OnComplete(() =>
+            {
+                fishRodTransform.DOLocalRotate(new Vector3(309.829834f,318.94632f,206.364349f), focusDuration / 3);
+                fishRodTransform.DOLocalMove(new Vector3(10.8649998f,9.84700012f,8.18500042f), focusDuration / 3)
+                    .SetDelay(focusDuration / 3)
+                    .SetEase(Ease.Linear);
+                fishRodTransform.DOLocalRotate(new Vector3(309.829834f,318.94632f,168.154434f), focusDuration / 3)
+                    .SetDelay(focusDuration / 3)
+                    .SetEase(Ease.Linear);
+            });
+
+            _sequence.SetEase(Ease.Flash);
+        }
+
+        private void UnfocusAnimation()
+        {
+            fishRodTransform?.DOKill();
+            _sequence?.Kill();
+
+            fishRodTransform.DOLocalMove(_startPosition, focusDuration);
+            fishRodTransform.DOLocalRotate(_startRotation, focusDuration);
         }
     }
 }
